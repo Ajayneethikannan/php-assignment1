b3c5cce6f2f47221cd2de5af484b09811b623117
Ajayneethikannan <35770004+Ajayneethikannan@users.noreply.github.com>
2018-03-19 09:12:41 +0530

    ###

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbServername="localhost";
-$dbUsername="root";
-$dbPassword="";
+$dbServername="192.168.121.187";
+$dbUsername="first_year";
+$dbPassword="first_year";
 $dbName="ajay";
 
 $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
